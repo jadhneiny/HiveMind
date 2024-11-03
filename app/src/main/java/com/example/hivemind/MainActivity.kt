18351348e69219b17555d069d423b9f4673a212c
dustycd <m.s.ghabra@gmail.com>
2024-11-03 18:21:03 +0200

    updated MainActivity

--- app/src/main/java/com/example/hivemind/MainActivity.kt
+++ app/src/main/java/com/example/hivemind/MainActivity.kt
@@ -3,56 +3,15 @@ package com.example.hivemind
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.hivemind.ui.theme.HiveMindTheme
 
 class MainActivity : ComponentActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-          val navController = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment)!!
-            .findNavController()
-
-        // Set up BottomNavigationView
-        binding.bottomNavigationView.setupWithNavController(navController)
-        
         setContent {
             HiveMindTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HiveMindTheme {
-        Greeting("Android")
     }
 }
